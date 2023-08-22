28e2d2558561a312db45aafc5af4a8d7b9ec3f76
Adam Castle <techdev3@hireandservice.com>
2023-08-22 16:21:46 +0200

    continued training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComonent } from './products/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComonent,
+    ConvertToSpaces
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
